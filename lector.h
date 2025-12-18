cbc724a18dfa3f23b8f7aac588c912064efc997f
zakinho22 <isaac@mizraji.net>
2025-12-18 11:13:43 -0300

    Update lector.h

--- lector.h
+++ lector.h
@@ -1,12 +1,12 @@
 #ifndef LECTOR_H
-define LECTOR_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define LECTOR_H
+
 #include "queries.h"
+
 #define MAX_LINE 1024
 #define DELIMITER ";"
 #define BLOCK 250
+
 typedef struct typevectorCDT * typevector;
 
 // creo la estructura para almacenar los tipos de reclamo
@@ -15,6 +15,8 @@ typevector newType(void);
 int readTypes(const char *filename, typevector types);
 //libero la estuctura de tipos de reclamo
 void freeTypes(typevector types);
-// leo los reclamos del archivo filename, los comparo con los tipos de reclamo y los añado a las estuctura de las queries
+/*  Leo los reclamos del archivo filename, 
+    los comparo con los tipos de reclamo y 
+    los añado a las estuctura de las queries*/
 int readRequest(const char *filename, typevector types, queryADT q);
 #endif
