6beb39284b7764dcc08efaa0f5e5cecad0ab57be
mburgos4 <mburgosbernasconi@itba.edu.ar>
2025-12-19 19:16:56 -0300

    Update queries.h

--- backend/queries.h
+++ backend/queries.h
@@ -21,12 +21,14 @@ typedef void (*Q2Visitor)(const char *borough, int hour, unsigned int count, voi
 typedef void (*Q3Visitor)(int quadLat, int quadLong, unsigned int rec, void *userData);
 typedef void (*Q4Visitor)(const char *borough, const char *agency, const char *type, int daysBetween, void *userData);
 typedef void (*Q5Visitor)(int quadLat, int quadLong, int year, int month, int recYTD, void *userData);
-queryADT newQueries(void);
-void addToQueries(queryADT q,const char * agency, const char * code, const char *name, const char * status, const char * borough, int year, int month, int day, int hour, double lat, double lon, int yMax, int yMin);
-void q1_foreach(queryADT q, Q1Visitor v, void *userData);
-void q2_foreach(queryADT q, Q2Visitor v, void *userData);
+queryADT newQueries(void); //crea 5 nuevas queries
+void addToQueries(queryADT q,const char * agency, const char * code,         //agrega un reclamo a todas las queries
+const char *name,const char * status, const char * borough, int year, 
+int month, int day, int hour, double lat, double lon, int yMax, int yMin);
+void q1_foreach(queryADT q, Q1Visitor v, void *userData);      //qN_foreach recibe una funcion QNVisitor que se va a definir en el main y 
+void q2_foreach(queryADT q, Q2Visitor v, void *userData);      //la ejecuta para toda la query
 void q3_foreach(queryADT q, Q3Visitor v, void *userData);
 void q4_foreach(queryADT q, Q4Visitor v, void *userData);
 void q5_foreach(queryADT q, Q5Visitor v, void *userData);
-void freeQueries(queryADT q);
+void freeQueries(queryADT q);                                //freeQueries libera todas las queries
 #endif
