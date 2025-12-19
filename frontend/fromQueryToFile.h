fc5c2e08fe0872201e5057cd548a8a6cf1e01d48
mburgos4 <mburgosbernasconi@itba.edu.ar>
2025-12-19 19:20:56 -0300

    Update fromQueryToFile.h

--- frontend/fromQueryToFile.h
+++ frontend/fromQueryToFile.h
@@ -2,7 +2,7 @@
 #define __FROM_QUERY_TO_FILE_H__
 #include "../backend/queries.h"
 int queriesToFile(queryADT q, 
-                const char *csvFile1, const char *htmlFile1, 
+                const char *csvFile1, const char *htmlFile1,             //convierte a cada query en un archivo csv y html
                 const char *csvFile2, const char *htmlFile2, 
                 const char *csvFile3, const char *htmlFile3, 
                 const char *csvFile4, const char *htmlFile4,
