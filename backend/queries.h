e785cc2676d765ee11c73e0622abc7b7bfe3fd58
zakinho22 <isaac@mizraji.net>
2025-12-19 11:28:35 -0300

    Update queries.h

--- backend/queries.h
+++ backend/queries.h
@@ -1,11 +1,22 @@
 #ifndef __QUERIES_H__
 #define __QUERIES_H__
-
+#if defined(CITY_NYC)
+#define CERRADO "Closed"
+#elif defined(CITY_CHICAGO)
+#define CERRADO "Completed"
+#else
+#error "Debe definirse CITY_NYC O CITY_CHICAGO"
+#endif
 #define HOURS 24
 #define BLOQUE 50
+#define MONTHS 12
+typedef struct q1Node *q1List;
+typedef struct q2Node *q2List;
+typedef struct q3Node *q3List;
+typedef struct q4Node *q4List;
+typedef struct q5Node *q5List;
 typedef struct queryCDT * queryADT;
 queryADT newQueries(void);
-void addToQueries(queryADT q,const char * agency, const char * code, const char *name, const char * status, const char * borough, int year, int month, int day, int hour, int quadLat, int quadLong, int yMax, int yMin);
+void addToQueries(queryADT q,const char * agency, const char * code, const char *name, const char * status, const char * borough, int year, int month, int day, int hour, double lat, double lon, int yMax, int yMin);
 void freeQueries(queryADT q);
-
 #endif
